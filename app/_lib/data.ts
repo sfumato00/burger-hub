ec9913aa630fd4781b7ea620d5fc73f6dd2665b9
sfumato00 <97776161+sfumato00@users.noreply.github.com>
2024-01-11 18:59:32 -0800

    refactor to enable CC and SC composition.

--- app/_lib/data.ts
+++ app/_lib/data.ts
@@ -1,8 +1,10 @@
+import { Product } from "@/app/_lib/definitions";
+
 export async function fetchProductData() {
   try {
     const response = await fetch(
       "https://sfumato00.github.io/json/data/products.json",
-      { cache: "no-store" },
+      { next: { revalidate: 3600 } },
     );
 
     const { products } = await response.json();
