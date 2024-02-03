8d8d57cb6bd7c331cc7be0ed6d654cf4caa360d3
sfumato00 <97776161+sfumato00@users.noreply.github.com>
2024-02-03 15:43:58 -0800

    feat: make shopping cart persist in localstorage

--- app/_context/shopping-cart-context.tsx
+++ app/_context/shopping-cart-context.tsx
@@ -1,27 +1,59 @@
 "use client";
 
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { ShoppingCartItem } from "@/app/_lib/definitions";
 
 interface ShoppingCartProviderProps {
   children: ReactNode;
 }
 
-interface ShoppingCartContextType {
+interface ShoppingCartContextProps {
+  isCartOpen: boolean;
   items: ShoppingCartItem[];
+  setCartOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setItems: React.Dispatch<React.SetStateAction<ShoppingCartItem[]>>;
 }
 
-const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(
-  undefined,
-);
+const ShoppingCartContext = createContext<ShoppingCartContextProps>({
+  isCartOpen: false,
+  items: [],
+  setCartOpen: () => {},
+  setItems: () => {},
+});
 
 export const ShoppingCartProvider: React.FC<ShoppingCartProviderProps> = ({
   children,
 }) => {
-  const [items, setItems] = useState<ShoppingCartItem[]>([]);
+  const [items, setItems] = useState<ShoppingCartItem[] | []>(() => {
+    if (typeof window === "undefined") {
+      return [];
+    }
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(items));
+    setIsCartOpen(items.length > 0);
+  }, [items]);
+
   return (
-    <ShoppingCartContext.Provider value={{ items, setItems }}>
+    <ShoppingCartContext.Provider
+      value={{
+        isCartOpen: isCartOpen,
+        items,
+        setCartOpen: setIsCartOpen,
+        setItems,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
