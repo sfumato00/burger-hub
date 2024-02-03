1cc1464dc4bed5584439182dbe2b39b12c84a974
sfumato00 <97776161+sfumato00@users.noreply.github.com>
2024-02-03 15:45:39 -0800

    fix: product list page now is static with 5 min revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Catalog from "@/app/_components/organisms/catalog";
 import GlobalNavBar from "@/app/_components/molecules/NavBar/global-nav-bar";
 
+export const revalidate = 300;
+export const dynamic = "force-static";
 export default function Home() {
   return (
     <main>
